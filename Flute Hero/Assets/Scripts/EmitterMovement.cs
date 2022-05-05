8dd5db131d9f3cfe6b03842320e59172b5659459
vanessahychen <77706531+matthew-mcc@users.noreply.github.com>
2022-05-05 14:59:17 -0600

    Adding Data Files and Changing Emitter Movement

--- Flute Hero/Assets/Scripts/EmitterMovement.cs
+++ Flute Hero/Assets/Scripts/EmitterMovement.cs
@@ -7,6 +7,9 @@ public class EmitterMovement : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private GameObject prefab;
     
+    string filePath = @"C:\GIT\Research-2022\20220316112102AbBelt.txt";
+
+    List<float> voltageFromFile = new List<float>();
 
     [SerializeField] private int numPoints;
     [SerializeField] private float timeBetweenPoints = 1f;
@@ -16,7 +19,9 @@ public class EmitterMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        readFile();
         StartCoroutine(spawner());
+        StartCoroutine(moveEmitter());
         
     }
     IEnumerator spawner(){
@@ -32,6 +37,14 @@ public class EmitterMovement : MonoBehaviour
         
 
     }
+    IEnumerator moveEmitter(){
+
+
+        foreach(var val in voltageFromFile){
+            rb.transform.position = new Vector2(rb.transform.position.x, val); //Needs to be scaled a bit, dependant on what lucie wants
+            yield return new WaitForSeconds(1/60);
+        }
+    }
     private void spawnPoint(){
         
         Vector2 spawnPos = rb.transform.position;
@@ -42,10 +55,29 @@ public class EmitterMovement : MonoBehaviour
         
 
     }
+    private void readFile(){
+        string[] lines = System.IO.File.ReadAllLines(filePath);
+        // foreach (string line in lines){
+        //     Debug.Log(line);
+        // }
+        for (int i = 1; i < 100; i++){ //change if u want diff nums of lines...
+            string temp = lines[i];
+            
+            string[] splitWords = temp.Split('	');
+            
+            
+           
+            float value = float.Parse(splitWords[1]);
+            voltageFromFile.Add(value);
+            
+        }
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
+        
         //TODO: Implement movement of the square based on data
     }
 }
