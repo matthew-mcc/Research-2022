71f66734edb99f792914fc39b8ad64a00c9f030e
vanessahychen <77706531+matthew-mcc@users.noreply.github.com>
2022-12-23 14:41:39 -0700

    Com Port

---
+++ Com-Port/Com-Port-Testing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO.Ports;
+
+
+public class Program
+{
+    
+    string InData;
+    static SerialPort myComPort;
+    public static void Main()
+    {
+        // Init Com Port
+        myComPort = new SerialPort("COM4", 19200, Parity.None, 8, StopBits.One);
+        myComPort.Handshake = Handshake.None;
+        myComPort.DtrEnable = true;
+
+        // Event Handler
+        myComPort.DataReceived += new SerialDataReceivedEventHandler(DataRecievedHandler);
+        myComPort.Open();
+        
+        Console.WriteLine("Press any Key to Continue");
+        Console.WriteLine();
+        Console.ReadKey();
+        myComPort.Close();
+        
+    }
+
+    static void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e){
+       
+        string indata = myComPort.ReadLine();
+        Console.WriteLine(indata);
+    }
+
+}
